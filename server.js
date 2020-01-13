2cb549bab39e73d2e6231812b6885503ecb86078
Victor Hermes <me.victorhermes@gmail.com>
2020-01-13 16:17:13 -0300

    fix

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
-import express from "express";
-const app = express();
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const fs = require("fs");
+//const five = require("johnny-five");
+
+//const board = new five.Board();
 
 app.get("/", (req, res) => {
-  res.send("Vamos começar a festa :)");
+  fs.readFile(__dirname + "/index.html", function(err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end("Error loading index.html");
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
 });
 
-app.listen(3000, () => {
+//board.on("ready", function() {
+//const led = new five.Led(13);
+//const relay = new five.Relay(10);
+io.on("connection", function(socket) {
+  socket.on("click-01", function() {
+    console.log("Socket.io escutando");
+  });
+});
+//});
+
+http.listen(3000, () => {
   console.log("Servidor rodando na porta 3000.");
 });
